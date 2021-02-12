456b00f151d48766bd3517301a08624af884d5e1
Сиднин Михаил <msidnin@softrust.ru>
2021-02-12 18:59:05 +0300

    Частичное заполнение формы

--- src/test/java/DirectrionFullfillmentTest.java
+++ src/test/java/DirectrionFullfillmentTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.util.Random;
 
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
@@ -14,24 +17,43 @@ public class DirectrionFullfillmentTest {
                 middlePath = "test2/directions;mkabId=2662334;tapId=2670514;backUrl=%2Ftap%2Fcard%2F;menuName=Вернуться%20в%20ТАП;moduleName=Тап/Laboratory/new?ticket=",
                 ticket = "hSqZx4lN38o%2BhW7%2B92nmww4lbjTAuCEt96ZbaHdmJdqb%2BrEQR79h8tfX0fyVbK8VbzA2qIJxhnCDyI1Jt2BDhx8rkenU4HtA7EB6XXXRmpwUcDVupdn1o3gQT7Fa4V2qXC73gzg%2BIIZcfbdD%2BDiZphG8utN6Rmd6JkpCnzXqN0fAvwW9s7wzp60Lp7JBQtrcX4miN%2FwiEGyoIolfBncEmeWagyWCTbhIhXncHFlAdCuKJBK2Jzl3Go9NxXuy4uPeENKUzccPPqYtbflvU3FF06JRB3PX1y2i61v7PRQTl5U8fEl1pesEoYhgJywKd7LgZy2CDDnod1fGxM%2BFqwTFen6dB%2BM%3D&MkabId=2662334&TapId=2670514&DocPrvdId=347&MisUrl=http:%2F%2F192.168.7.54%2Fmis%2Ftest2&ReturnUrl=http:%2F%2F192.168.7.54%2Fmis%2Ftest2%2FSchedule&newStyle=1";
 
+        String directionPurpose[] ={"1 - Профилактическое исследование","2 - Диагностическое исследование"};
+
+        Random random = new Random();
+        int seed, i;
+
+        seed = random.nextInt(10);
+
+        if (seed <= 10)
+             {
+                 i = 0;
+             }
+        else
+             {
+                 i = 1;
+             }
+        //-----------------------------------------------------------------------------------------------------------
         open(ipAddress + middlePath + ticket);
-
-        //$("#mat-input-186").val("про").pressEnter();
-
-        //$("[ng-reflect-placeholder='Цель исследования']").val("acsaswwqf");
-        //$x("//input[@class='mat-input-element']").click();
 
 
         $(byAttribute("placeholder","Цель исследования")).click();
-        $("#mcdk-overlay-2").click();
-        $(byAttribute("class","mat-option-ripple")).click();
+        $(byAttribute("title","1 - Профилактическое исследование")).click();
 
+        $(byAttribute("placeholder","Причина направления")).click();
+        $(byAttribute("title","2 - Самообращение профилакт")).click();
 
-        //$x("//input[@id='mat-checkbox-1-input']").click();
+        $(byAttribute("placeholder","МКБ-10")).click();
+        $(byAttribute("title","A01.1 - Паратиф A")).click();
 
-        $(byText("Цель исследования")).val("asc");
-        $(byClassName("custom-check-box")).click();
+        $(byAttribute("formcontrolname","comment")).setValue("test");
 
+        $(byAttribute("placeholder","Исследование")).setValue("флюоро").pressEnter();
+
+        $(byAttribute("ng-reflect-message","A06.09.006 - Флюорография легк")).click();
+
+        $($x("//button[text()='Добавить'])")).click();
+
+        $($x("//app-directions[@class='ng-star-inserted']")).click();
     }
 
 
